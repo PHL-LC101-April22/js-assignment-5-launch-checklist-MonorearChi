253a7dd7bf1a0c69c877e97aeb223512344cc353
MonorearChi <chimonorear@gmail.com>
2022-06-23 22:32:24 -0400

    added window and alert validations

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,33 +2,45 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
+window.addEventListener("load", function () {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        let pilotName = document.querySelector("input[name=pilot]");
+        let coPilotName = document.querySelector("input[name=coPilot]");
+        let fuelLevel = docuement.querySelector("input[name=fuelLevel]");
+        let cargoMass = document.querySelector("input[name=cargo]");
+        alert("All fields are required!");
+
+    });
+});
+
 function validateInput(testInput) {
-   
+
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function (response) {
+    });
 
     return planetsReturned;
 }
@@ -39,5 +51,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
